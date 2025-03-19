3f49e517ae764c4ea5c141ae6af23694c8cbc481
AidenYav <yavaiden@gmail.com>
2025-03-18 18:02:00 -0700

    Implemented Drive Systems. Began working on Linear Slide subsystem.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstTest.java
@@ -2,7 +2,9 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 
 @TeleOp(name="FirstTest")
@@ -15,16 +17,41 @@ public class FirstTest extends LinearOpMode {
         DcMotorEx backLeft = hardwareMap.get(DcMotorEx.class,"backLeft");
         DcMotorEx frontRight = hardwareMap.get(DcMotorEx.class,"frontRight");
         DcMotorEx backRight = hardwareMap.get(DcMotorEx.class,"backRight");
+
+
+        DcMotorEx linearSlide = hardwareMap.get(DcMotorEx.class,"linenerSlide");
+        linearSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        linearSlide.setTargetPosition(linearSlide.getCurrentPosition());
+        linearSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        linearSlide.setPower(0.5);
+
+
+
+
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+        frontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
         waitForStart();
         //Start
         while(opModeIsActive()){
             double forward = gamepad1.left_stick_y;
-            double rotate = gamepad1.right_stick_x;
+            double rotate = -gamepad1.right_stick_x;
             double strafe = gamepad1.left_stick_x;
-            frontLeft.setPower(0.5);
-            frontRight.setPower(0.5);
-            backLeft.setPower(0.5);
-            backRight.setPower(0.5);
+
+
+            frontLeft.setPower(forward + rotate - strafe);
+            frontRight.setPower(forward - rotate - strafe);
+            backLeft.setPower(forward + rotate + strafe);
+            backRight.setPower(forward - rotate + strafe);
+
+            double linearSlideControl = gamepad1.left_trigger - gamepad1.right_trigger;
+            linearSlide.setTargetPosition(linearSlide.getCurrentPosition() + (int) linearSlideControl);
         }
 
     }
